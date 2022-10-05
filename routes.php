833a59cc4fb706f2820b14e9d319291e40d2ee4d
deondazy <deondazy@gmail.com>
2022-10-05 04:27:54 +0100

    Initial commit

---
+++ routes.php
@@ -0,0 +1,45 @@
+<?php
+
+require_once __DIR__ . '/bootstrap.php';
+
+require_once("{$_SERVER['DOCUMENT_ROOT']}/router.php");
+
+// ##################################################
+// ##################################################
+// ##################################################
+
+// Static GET
+// In the URL -> http://localhost
+// The output -> Index
+get('/', 'views/index.php');
+
+// Dynamic GET. Example with 1 variable
+// The $id will be available in user.php
+get('/user/$id', 'user.php');
+
+// Dynamic GET. Example with 2 variables
+// The $name will be available in user.php
+// The $last_name will be available in user.php
+get('/user/$name/$last_name', 'user.php');
+
+// Dynamic GET. Example with 2 variables with static
+// In the URL -> http://localhost/product/shoes/color/blue
+// The $type will be available in product.php
+// The $color will be available in product.php
+get('/product/$type/color/:color', 'product.php');
+
+// Dynamic GET. Example with 1 variable and 1 query string
+// In the URL -> http://localhost/item/car?price=10
+// The $name will be available in items.php which is inside the views folder
+get('/item/$name', 'views/items.php');
+
+
+// ##################################################
+// ##################################################
+// ##################################################
+// any can be used for GETs or POSTs
+
+// For GET or POST
+// The 404.php which is inside the views folder will be called
+// The 404.php has access to $_GET and $_POST
+any('/404','views/404.php');
---
+++ src/Router.php
@@ -0,0 +1,8 @@
+<?php 
+
+namespace Deondazy\Core;
+
+class Router 
+{
+    
+}
