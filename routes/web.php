a81324e3b520cef7a9c06e37fa214d2fb1eb12ca
deondazy <deondazy@gmail.com>
2022-10-08 00:43:40 +0100

    Update routes

--- routes/web.php
+++ routes/web.php
@@ -5,5 +5,10 @@ use Deondazy\Core\Controllers\HomepageController;
 $router->get('/', HomepageController::class . '@index');
 
 $router->notFoundHandler(function() {
-    require_once __DIR__ . '/../views/404.html';
+    require_once CORE_ROOT . '/views/404.html';
+});
+
+// TODO: Remove this
+$router->get('/test', function() {
+    require_once CORE_ROOT . '/test.php';
 });
