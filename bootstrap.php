bf2156edda4d1b501cf3d48889ee1927df838576
deondazy <deondazy@gmail.com>
2022-10-08 00:42:46 +0100

    Update bootstrap.php
    Remove all constant definitions from this file

--- bootstrap.php
+++ bootstrap.php
@@ -1,34 +1,40 @@
 <?php
 /**
  * The Deondazy Core Bootstrap File
- *
+ * 
+ * This file sets up all the required files and constants
+ * 
+ * @package Deondazy\Core
+ * @version 1.0.0
  */
 
-use Whoops\Run;
-use Whoops\Handler\PrettyPageHandler;
-use Deondazy\Core\Exceptions\InvalidRequirementException;
+// File cannot be accessed directly
+// if (!defined('CORE_ROOT')) {
+//     header('HTTP/1.0 403 Forbidden');
+//     exit;
+// }
 
-// Define the app version
-define('CORE_VERSION', '1.0.0');
-
-// Define required PHP version
-define('CORE_PHP', '7.4');
-
-// Define installation root path
-define('CORE_ROOT', __DIR__);
-
-// Define Public root path
-define('CORE_PUBLIC', CORE_ROOT . '/public');
+// Require the constants file
+require_once __DIR__ . '/constants.php';
 
 // Require Autoloader
 require_once CORE_ROOT . '/vendor/autoload.php';
 
 // Use Whoops for error handling
-$whoops = new Run;
-$whoops->pushHandler(new PrettyPageHandler);
+$whoops = new Whoops\Run;
+$whoops->pushHandler(new Whoops\Handler\PrettyPageHandler);
 $whoops->register();
 
 // We need to check if the PHP version is compatible with the app
 if (version_compare(PHP_VERSION, CORE_PHP, '<')) {
-    throw new InvalidRequirementException('PHP version ' . CORE_PHP . ' or higher is required. You are using PHP version ' . PHP_VERSION);
+    throw new Deondazy\Core\Exceptions\InvalidRequirementException(
+        'PHP version ' . CORE_PHP . ' or higher is required. You are using PHP version ' . PHP_VERSION
+    );
 }
+
+// Load environment variables
+$dotenv = Dotenv\Dotenv::createImmutable(CORE_ROOT);
+$dotenv->safeLoad();
+
+// Load Database connection
+$database = Deondazy\Core\Database::instance()->connect("mysql:host={$_ENV['DB_HOST']};dbname={$_ENV['DB_NAME']}", $_ENV['DB_USER'], $_ENV['DB_PASSWORD']);
