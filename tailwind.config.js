cc5bcdf819ce92bac3ac7e9cb9fce73440ab81d4
Deon Dazy <deondazy@Deons-MacBook-Pro.local>
2022-10-27 04:18:00 +0100

    Move views to app/Views

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,7 @@ const defaultTheme = require('tailwindcss/defaultTheme');
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './views/**/*.{html, js}',
+    './app/Views/**/*.{html, js}',
   ],
   theme: {
     extend: {
