d5809d50bf5a800c751fd15742ef9446ab20606a
Deon Dazy <deondazy@Deons-MacBook-Pro.local>
2022-10-27 04:25:51 +0100

    Update index.php
    Move Twig template engine loader to controller

--- public/index.php
+++ public/index.php
@@ -1,19 +1,10 @@
 <?php
 
-require_once __DIR__ . '/../bootstrap.php';
-
 use Deondazy\Core\Router;
-use Twig\Environment;
-use Twig\Loader\FilesystemLoader;
+
+require_once __DIR__.'/../bootstrap/app.php';
 
 $router = new Router;
-
-// Load Twig template engine and pass it to the router
-$loader = new FilesystemLoader(CORE_ROOT . '/views');
-$twig = new Environment($loader, [
-    'cache' => CORE_ROOT . '/cache',
-    'debug' => true,
-]);
 
 require_once CORE_ROOT . '/routes/web.php';
 
