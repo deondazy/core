04850384ce6b2364e2d8ff2f80a4831c3b2b200d
Deon Dazy <deondazy@Deons-MacBook-Pro.local>
2022-11-04 07:21:16 +0100

    :construction: chore: remove root test.php route

--- routes/web.php
+++ routes/web.php
@@ -7,8 +7,3 @@ $router->get('/', HomeController::class . '@index');
 $router->notFoundHandler(function() {
     require_once CORE_ROOT . '/app/Views/404.html';
 });
-
-// TODO: Remove this route
-$router->get('/test', function() {
-    include CORE_ROOT . '/test.php';
-});
