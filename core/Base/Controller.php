d5587de96dd688d8402a74cd20d89a4f9554717a
Deon Dazy <deondazy@Deons-MacBook-Pro.local>
2022-11-04 06:48:41 +0100

    :recycle: refactor: update controller to load twig from base view

---
+++ core/Base/Controller.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Deondazy\Core\Base;
+
+use Deondazy\Core\Base\View;
+use Twig\Error\LoaderError;
+use Twig\Error\RuntimeError;
+use Twig\Error\SyntaxError;
+use Deondazy\Core\Config\Exceptions\FileNotFoundException;
+
+class Controller
+{
+    /**
+     * The view instance
+     *
+     * @var View
+     */
+    private $view;
+
+    public function __construct()
+    {
+        $this->view = new View;
+    }
+
+    /**
+     * Render a view file template
+     *
+     * @param string $template
+     * @param array $data
+     *
+     * @return void
+     *
+     * @throws LoaderError
+     * @throws RuntimeError
+     * @throws SyntaxError
+     * @throws FileNotFoundException
+     */
+    public function render(string $template, array $data = []): void
+    {
+        $this->view->render($template, $data);
+    }
+}
