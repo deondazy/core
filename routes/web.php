18fef08378c251d8cdad9120ccd6c2219c5f6618
Deon Dazy <deondazy@Deons-MacBook-Pro.local>
2022-11-05 06:45:30 +0100

    :construction: refactor: change controller string to array

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,7 @@
 
 use Deondazy\App\Controllers\HomeController;
 
-$router->get('/', HomeController::class . '@index');
+$router->get('/', [HomeController::class, 'index']);
 
 $router->notFoundHandler(function() {
     require_once CORE_ROOT . '/app/Views/404.html';
