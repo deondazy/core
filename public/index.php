af67b4700027bbbbba2e68d4d46537890dea0243
Deon Okonkwo <deondazy@gmail.com>
2023-06-04 23:27:53 +0100

    :recycle: refactor: get app from the bootstrap app

--- public/index.php
+++ public/index.php
@@ -2,14 +2,10 @@
 
 declare(strict_types = 1);
 
-use DI\Bridge\Slim\Bridge;
-
 // Require Autoloader
 require __DIR__ . '/../vendor/autoload.php';
 
-require_once __DIR__ . '/../bootstrap/app.php';
-
-$app = Bridge::create($container);
+$app = require_once __DIR__ . '/../bootstrap/app.php';
 
 require_once CORE_ROOT . '/routes/web.php';
 
