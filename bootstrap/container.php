d09a5c3d6597f9cebe1a4bc6157552103302711c
Deon Okonkwo <deondazy@gmail.com>
2023-06-05 22:10:08 +0100

    :sparkles: feat: add twig to container

--- bootstrap/container.php
+++ bootstrap/container.php
@@ -2,7 +2,9 @@
 
 declare(strict_types = 1);
 
+use Slim\Views\Twig;
 use Slim\Psr7\Response;
+use Psr\Container\ContainerInterface;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
 use Slim\Factory\ServerRequestCreatorFactory;
@@ -15,5 +17,15 @@ return [
 
     ResponseInterface::class => function () {
         return new Response();
+    },
+
+    Twig::class => function (ContainerInterface $container) {
+        $config = $container->get(Config::class)->get('views.twig');
+    
+        $twig = Twig::create(__DIR__ . '/../app/Views', $config);
+    
+        // $twig->addExtension(new \Twig\Extension\DebugExtension());
+    
+        return $twig;
     },
 ];
