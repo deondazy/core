8474c806988ec69633d7d908e3801b5c9d55c6a6
Deon Okonkwo <deondazy@gmail.com>
2023-06-05 22:05:59 +0100

    :fire: refactor: remove unnecessary code

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -9,31 +9,12 @@
  * @version 1.0.0
  */
 
-use Whoops\Run;
 use Dotenv\Dotenv;
 use DI\ContainerBuilder;
-use DI\Bridge\Slim\Bridge;
-use Symfony\Component\Yaml\Yaml;
-use Whoops\Handler\PrettyPageHandler;
 
 // Load environment variables
 Dotenv::createImmutable(dirname(dirname(__FILE__)))->safeLoad();
 
 $containerBuilder = new ContainerBuilder();
-$containerBuilder->addDefinitions(__DIR__ . '/../config/container.php');
-$container = $containerBuilder->build();
-
-// Require the constants file
-require_once __DIR__ . '/constants.php';
-
-// Require the config file
-$config = Yaml::parseFile(CORE_CONFIG . '/app.yaml');
-
-// Use Whoops for error handling if debug is enabled
-if ($config['debug']) {
-    $whoops = new Run();
-    $whoops->pushHandler(new PrettyPageHandler());
-    $whoops->register();
-}
-
-return Bridge::create($container);
+$containerBuilder->addDefinitions(__DIR__ . '/container.php');
+return $containerBuilder->build();
