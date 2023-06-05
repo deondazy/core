3e9dcd94ea1dff0ae0392c5da9b01fccee55691b
Deon Okonkwo <deondazy@gmail.com>
2023-06-05 22:09:17 +0100

    :sparkles: feat: add responseInterface to container

--- bootstrap/container.php
+++ bootstrap/container.php
@@ -2,6 +2,8 @@
 
 declare(strict_types = 1);
 
+use Slim\Psr7\Response;
+use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
 use Slim\Factory\ServerRequestCreatorFactory;
 
@@ -9,5 +11,9 @@ return [
     ServerRequestInterface::class => function () {
         $serverRequestCreator = ServerRequestCreatorFactory::create();
         return $serverRequestCreator->createServerRequestFromGlobals();
+    },
+
+    ResponseInterface::class => function () {
+        return new Response();
     },
 ];
