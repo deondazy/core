e79a22ed71392edf9f821716403f295c56dc1791
Deon Okonkwo <deondazy@gmail.com>
2023-06-05 22:03:49 +0100

    :recycle: refactor: remove request&response params

--- app/Controllers/HomeController.php
+++ app/Controllers/HomeController.php
@@ -3,15 +3,11 @@
 namespace Deondazy\App\Controllers;
 
 use Deondazy\Core\Base\Controller;
-use Psr\Http\Message\ResponseInterface as Response;
-use Psr\Http\Message\ServerRequestInterface as Request;
 
 class HomeController extends Controller
 {
-    public function index(Request $request, Response $response)
-    {
-        $this->render('index');
-        
-        return $response;
+    public function index()
+    {   
+        return $this->render('index.html', ['home' => 'My Home Page']);
     }
 }
