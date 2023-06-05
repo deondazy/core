dc513caeed201268c50bd788411b177399ed9dee
Deon Okonkwo <deondazy@gmail.com>
2023-06-05 22:20:20 +0100

    :recycle: refactor: use container and whoops

--- public/index.php
+++ public/index.php
@@ -2,11 +2,25 @@
 
 declare(strict_types = 1);
 
+use DI\Bridge\Slim\Bridge;
+use Slim\Views\TwigMiddleware;
+use Zeuxisoo\Whoops\Slim\WhoopsMiddleware;
+
 // Require Autoloader
 require __DIR__ . '/../vendor/autoload.php';
 
-$app = require_once __DIR__ . '/../bootstrap/app.php';
+$container = require_once __DIR__ . '/../bootstrap/app.php';
 
-require_once CORE_ROOT . '/routes/web.php';
+$app = Bridge::create($container);
+
+// Add Routing Middleware
+$app->addRoutingMiddleware();
+
+// Add Twig-View Middleware
+$app->add(TwigMiddleware::createFromContainer($app));
+
+$app->add(new WhoopsMiddleware());
+
+require_once __DIR__ . '/../routes/web.php';
 
 $app->run();
