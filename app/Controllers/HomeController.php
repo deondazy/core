38781debefa15b4ee8b88140e02e48422aa8cbab
Deon Okonkwo <deondazy@gmail.com>
2023-06-06 20:50:15 +0100

    :recycle: refactor: add return type

--- app/Controllers/HomeController.php
+++ app/Controllers/HomeController.php
@@ -3,10 +3,11 @@
 namespace Deondazy\App\Controllers;
 
 use Deondazy\Core\Base\Controller;
+use Psr\Http\Message\ResponseInterface;
 
 class HomeController extends Controller
 {
-    public function index()
+    public function index(): ResponseInterface
     {   
         return $this->render('index.html', ['home' => 'My Home Page']);
     }
