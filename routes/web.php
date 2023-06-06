95673899eb0d6356eb57856aac5abaa84a3291cd
Deon Okonkwo <deondazy@gmail.com>
2023-06-06 21:19:34 +0100

    :recycle: refactor: remove route param

--- routes/web.php
+++ routes/web.php
@@ -5,5 +5,5 @@ use Deondazy\App\Controllers\HomeController;
 
 $app->get('/', [HomeController::class, 'index'])->setName('home');
 
-$app->get('/sign-in/{username}', [AuthController::class, 'login'])->setName('login');
+$app->get('/sign-in', [AuthController::class, 'login'])->setName('login');
 $app->get('/register', [AuthController::class, 'register'])->setName('register');
