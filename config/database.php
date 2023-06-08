0858f55d61001ec751e83fffca11fcbdf2e47e83
Deon Okonkwo <deondazy@gmail.com>
2023-06-08 06:13:55 +0100

    :wrench: chore: add database config file

---
+++ config/database.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types = 1);
+
+return [
+
+    'mysql' => [
+        'driver'   => 'pdo_mysql',
+        'host'     => $_ENV['DB_HOST'] ?? 'localhost',
+        'port'     => $_ENV['DB_PORT'] ?? 3306,
+        'dbname'   => $_ENV['DB_NAME'],
+        'user'     => $_ENV['DB_USER'],
+        'password' => $_ENV['DB_PASSWORD'],
+        'charset'  => 'utf8mb4',
+    ],
+];
