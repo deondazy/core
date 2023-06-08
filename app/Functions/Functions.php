1b3e97779aec6b1d8cccdf0fb51ec5ee9947cbfe
Deon Okonkwo <deondazy@gmail.com>
2023-06-08 06:20:51 +0100

    :construction: chore: add functions helper

---
+++ app/Functions/Functions.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types = 1);
+
+use Deondazy\Core\Config;
+
+if (!function_exists('config')) {
+    
+    function config($key)
+    {
+        // return $container->get(Config::class)->get($key);
+    }
+}
