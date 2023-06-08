6a8146eee8ac10b91059fc9fb036fc85e1a45ec9
Deon Okonkwo <deondazy@gmail.com>
2023-06-08 12:41:57 +0100

    :recycle: refactor: get app factory from the container

--- public/index.php
+++ public/index.php
@@ -2,23 +2,8 @@
 
 declare(strict_types = 1);
 
-use DI\Bridge\Slim\Bridge;
-use Slim\Views\TwigMiddleware;
-use Zeuxisoo\Whoops\Slim\WhoopsMiddleware;
-
-ini_set('display_errors', true);
-
-$container = require_once __DIR__ . '/../bootstrap/app.php';
-
-$app = Bridge::create($container);
-
-// Add Routing Middleware
-$app->addRoutingMiddleware();
-
-// Add Twig-View Middleware
-$app->add(TwigMiddleware::createFromContainer($app));
-
-$app->add(new WhoopsMiddleware());
+$app = (require_once __DIR__ . '/../bootstrap/app.php')
+    ->get('AppFactory');
 
 require_once __DIR__ . '/../routes/web.php';
 
