a247edc351eef3af097bdd838641d59f41d089f6
Deon Okonkwo <deondazy@gmail.com>
2023-06-08 12:14:59 +0100

    :truck: chore: move entity timestamps trait to app dir

---
+++ app/Database/Entities/Traits/HasTimestamps.php
@@ -0,0 +1,50 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace Deondazy\Core\Entity\Traits;
+
+use DateTime;
+use Doctrine\ORM\Mapping\Column;
+use Doctrine\ORM\Mapping\PreUpdate;
+use Doctrine\ORM\Mapping\PrePersist;
+use Doctrine\Persistence\Event\LifecycleEventArgs;
+
+trait HasTimestamps
+{
+    #[Column(name: 'created_at')]
+    private DateTime $createdAt;
+
+    #[Column(name: 'updated_at')]
+    private DateTime $updatedAt;
+
+    #[PrePersist, PreUpdate]
+    public function updateTimestamps(LifecycleEventArgs $args): void
+    {
+        if (! isset($this->createdAt)) {
+            $this->createdAt = new DateTime();
+        }
+
+        $this->updatedAt = new DateTime();
+    }
+
+    public function getCreatedAt(): DateTime
+    {
+        return $this->createdAt;
+    }
+
+    public function setCreatedAt(DateTime $createdAt)
+    {
+        $this->createdAt = $createdAt;
+    }
+
+    public function getUpdatedAt(): DateTime
+    {
+        return $this->updatedAt;
+    }
+
+    public function setUpdatedAt(DateTime $updatedAt)
+    {
+        $this->updatedAt = $updatedAt;
+    }
+}
