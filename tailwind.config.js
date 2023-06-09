39d41c31e523d56a754f469c5ad38424dfa5590b
Deon Okonkwo <deondazy@gmail.com>
2023-06-09 22:45:27 +0100

    :wrench: chore: update tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,10 @@
-const defaultTheme = require('tailwindcss/defaultTheme');
-
 /** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: [
-    './app/Views/**/*{.html, .twig, .html.twig}',
-  ],
-  theme: {
-    extend: {
-        fontFamily: {
-            sans: ['Lato', ...defaultTheme.fontFamily.sans],
-        },
+export default {
+    content: [
+      './app/Views/**/*{.html, .twig, .html.twig}',
+    ],
+    theme: {
+      extend: {},
     },
-  },
-  plugins: [],
+    plugins: [],
 }
