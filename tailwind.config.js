2f18283f525c6bb01d5c4694743bb31c1715554c
Deon Okonkwo <deondazy@gmail.com>
2023-06-09 22:17:26 +0100

    :wrench: refactor: add support for other twig file extensions

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,7 @@ const defaultTheme = require('tailwindcss/defaultTheme');
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './app/Views/**/*.{html, js}',
+    './app/Views/**/*{.html, .twig, .html.twig}',
   ],
   theme: {
     extend: {
