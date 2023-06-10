94baf7827ab634ab94b9451f85c88f681deb0598
Deon Okonkwo <deondazy@gmail.com>
2023-06-10 01:15:29 +0100

    :wrench: chore: add views dir to paths config

--- config/paths.php
+++ config/paths.php
@@ -6,6 +6,8 @@ return [
 
     'public_dir' => __DIR__ . '/../public',
 
+    'views_dir' => __DIR__ . '/../app/Views',
+
     'entity_dir' => [
         __DIR__ . '/../app/Database/Entities',
     ],
