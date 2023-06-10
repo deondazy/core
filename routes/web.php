e635132ae47d8e49bf03e1c9107335f9c63dd19d
Deon Okonkwo <deondazy@gmail.com>
2023-06-10 18:31:36 +0100

    :wrench: chore: add post registration route

--- routes/web.php
+++ routes/web.php
@@ -6,5 +6,6 @@ use Deondazy\App\Controllers\HomeController;
 $app->get('/', [HomeController::class, 'index'])->setName('home');
 
 $app->get('/login', [AuthController::class, 'login'])->setName('login');
-$app->get('/register', [AuthController::class, 'register'])->setName('register');
+$app->get('/register', [AuthController::class, 'showRegistrationForm'])->setName('register');
+$app->post('/register', [AuthController::class, 'register'])->setName('register.post');
 $app->get('/forgot-password', [AuthController::class, 'forgotPassword'])->setName('forgot-password');
