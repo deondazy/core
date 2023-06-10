6fe9a6a39847d723956a1582542a2c148e5a29e6
Deon Okonkwo <deondazy@gmail.com>
2023-06-10 01:18:47 +0100

    :recycle: refactor: rebuild the twig vite extension

--- src/View/ViteExtension.php
+++ src/View/ViteExtension.php
@@ -10,40 +10,100 @@ use Twig\Extension\AbstractExtension;
 
 class ViteExtension extends AbstractExtension
 {
-
     public function __construct(
+        private Config $config,
         private bool $isDev,
-        private array $manifest,
-        private Config $config)
-    {
-        if (!$this->isDev) {
-            $this->manifest = json_decode(
-                file_get_contents(
-                    $config->get('paths.public_dir') . '/build/manifest.json'
-                ), true
-            );
-        }
-    }
+        private string $manifest,
+        private string $devServerHost,
+        private int $devServerPort
+    ) {}
 
     public function getFunctions()
     {
         return [
-            new TwigFunction('vite_asset', [$this, 'getViteAsset']),
+            new TwigFunction(
+                'vite',
+                [$this, 'getViteAsset'],
+                ['is_safe' => ['html']]
+        ),
         ];
     }
 
-    public function getViteAsset(string $asset): string
+    public function getViteAsset(array $assets): string
     {
         if ($this->isDev) {
-            // Development mode: return the asset from the Vite development server
-            return "http://127.0.0.1:4000/$asset";
+            return $this->renderDevAssets($assets);
         } else {
-            // Production mode: use the manifest file to get the asset URL
-            if (isset($this->manifest[$asset])) {
-                return "/build/{$this->manifest[$asset]}";
-            } else {
-                return "/build/$asset"; // Fallback if the asset is not found in the manifest
+            $manifest = json_decode(
+                file_get_contents($this->manifest),
+                true
+            );
+
+            $html = '';
+            foreach ($assets as $asset) {
+                $html .= $this->renderProductionAsset($asset, $manifest);
             }
+
+            return $html;
+        }
+    }
+
+    private function renderDevAssets(array $assets): string
+    {
+        $html = $this->getViteModule();
+
+        foreach ($assets as $asset) {
+            $html .= match ($this->getFileExtension($asset)) {
+                'js' => $this->renderScriptTag("http://{$this->devServerHost}:{$this->devServerPort}/$asset"),
+                'css' => $this->renderLinkTag("http://{$this->devServerHost}:{$this->devServerPort}/$asset"),
+                default => '',
+            };
+        }
+
+        return $html;
+    }
+
+    private function renderProductionAsset(string $asset, array $manifest): string
+    {
+        if (isset($manifest[$asset])) {
+            $file = $manifest[$asset]['file'];
+            $url = $this->config->get('app.url') . "/build/$file";
+
+            return match ($this->getFileExtension($asset)) {
+                'js' => $this->renderScriptTag($url),
+                'css' => $this->renderLinkTag($url),
+                default => '',
+            };
+        }
+
+        return '';
+    }
+
+    private function getFileExtension(string $filename): string
+    {
+        $extension = pathinfo($filename, PATHINFO_EXTENSION);
+        return strtolower($extension);
+    }
+
+    private function renderScriptTag(string $src): string
+    {
+        return "<script src=\"$src\"></script>";
+    }
+
+    private function renderLinkTag(string $href): string
+    {
+        return "<link rel=\"stylesheet\" href=\"$href\">";
+    }
+
+    public function getViteModule(): string
+    {
+        if ($this->isDev) {
+            return "<script 
+                type=\"module\"
+                src=\"http://{$this->devServerHost}:{$this->devServerPort}/@vite/client\">
+                </script>";
+        } else {
+            return '';
         }
     }
 }
