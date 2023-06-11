4e2844520ff654653355eb44ae0397cb73a05605
Deon Okonkwo <deondazy@gmail.com>
2023-06-11 15:23:07 +0100

    :wrench: chore: add login routes

--- routes/web.php
+++ routes/web.php
@@ -5,7 +5,8 @@ use Deondazy\App\Controllers\HomeController;
 
 $app->get('/', [HomeController::class, 'index'])->setName('home');
 
-$app->get('/login', [AuthController::class, 'login'])->setName('login');
+$app->get('/login', [AuthController::class, 'showLoginForm'])->setName('login');
+$app->post('/login', [AuthController::class, 'login'])->setName('login.post');
 $app->get('/register', [AuthController::class, 'showRegistrationForm'])->setName('register');
 $app->post('/register', [AuthController::class, 'register'])->setName('register.post');
 $app->get('/forgot-password', [AuthController::class, 'forgotPassword'])->setName('forgot-password');
