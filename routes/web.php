00800cac934738e737861d5c9ad53741537f42d7
Deon Okonkwo <deondazy@gmail.com>
2023-06-11 15:23:55 +0100

    :sparkles: feat: add a middleware auth protected route group

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,8 @@
 
 use Deondazy\App\Controllers\AuthController;
 use Deondazy\App\Controllers\HomeController;
+use Deondazy\App\Controllers\DashboardController;
+use Deondazy\App\Middleware\RequireAuthentication;
 
 $app->get('/', [HomeController::class, 'index'])->setName('home');
 
@@ -10,3 +12,7 @@ $app->post('/login', [AuthController::class, 'login'])->setName('login.post');
 $app->get('/register', [AuthController::class, 'showRegistrationForm'])->setName('register');
 $app->post('/register', [AuthController::class, 'register'])->setName('register.post');
 $app->get('/forgot-password', [AuthController::class, 'forgotPassword'])->setName('forgot-password');
+
+$app->group('/dashboard', function ($app) {
+    $app->get('', [DashboardController::class, 'index'])->setName('dashboard.index');
+})->add(RequireAuthentication::class);
