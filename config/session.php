ae74276e414cec7b02d3e56326f4ef7b7e76a5cb
Deon Okonkwo <deondazy@gmail.com>
2023-06-11 15:24:35 +0100

    :wrench: chore: add session config file

---
+++ config/session.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types = 1);
+
+return [
+
+    'name' => 'core',
+    'lifetime' => 7200,
+    'path' => null,
+    'domain' => null,
+    'secure' => false,
+    'httponly' => true,
+    'cache_limiter' => 'nocache',
+];
