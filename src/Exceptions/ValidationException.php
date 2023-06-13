e823a5f058364a26c6a8aeb284376b77b32bae5a
Deon Okonkwo <deondazy@gmail.com>
2023-06-13 04:34:08 +0100

    :wrench: chore: add validation exception

---
+++ src/Exceptions/ValidationException.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Deondazy\Core\Exceptions;
+
+use Throwable;
+
+class ValidationException extends \RuntimeException
+{
+    public function __construct(
+        public readonly array $errors,
+        string $message = "Validation error(s)", 
+        int $code = 422, 
+        ?Throwable $previous = null
+    ) {
+        parent::__construct($message, $code, $previous);
+    }
+}
