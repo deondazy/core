27ccf2f7a78fcda633022d45b5ea6e9171e4f4cd
Deon Okonkwo <deondazy@gmail.com>
2023-06-13 19:54:58 +0100

    :recycle: refactor: use session interface

--- app/Middleware/ValidationExceptionMiddleware.php
+++ app/Middleware/ValidationExceptionMiddleware.php
@@ -1,29 +1,36 @@
 <?php
 
-declare(strict_types = 1);
+declare(strict_types=1);
 
 namespace Deondazy\App\Middleware;
 
+use Odan\Session\SessionInterface;
+use Psr\Http\Server\MiddlewareInterface;
 use Psr\Http\Message\ResponseFactoryInterface;
 use Deondazy\Core\Exceptions\ValidationException;
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
-use Psr\Http\Server\MiddlewareInterface;
 use Psr\Http\Server\RequestHandlerInterface as RequestHandler;
 
 class ValidationExceptionMiddleware implements MiddlewareInterface
 {
-    public function __construct(private ResponseFactoryInterface $responseFactory)
-    {
+    public function __construct(
+        private ResponseFactoryInterface $responseFactory,
+        private SessionInterface $session
+    ) {
     }
 
-    public function process(Request $request, RequestHandler $handler): Response
-    {
+    public function process(
+        Request $request,
+        RequestHandler $handler
+    ): Response {
         try {
             return $handler->handle($request);
         } catch (ValidationException $e) {
             $response = $this->responseFactory->createResponse();
-            
+
+            $this->session->set('errors',  $e->errors);
+
             return $response->withStatus(302)
                 ->withHeader('Location', $request->getUri()->getPath());
         }
