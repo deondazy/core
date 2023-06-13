e3fdb4d3da747799468c70674fb0306ed1ef467a
Deon Okonkwo <deondazy@gmail.com>
2023-06-13 19:54:16 +0100

    :sparkles: feat: add form error flash middleware

---
+++ app/Middleware/FlashValidationErrorMiddleware.php
@@ -0,0 +1,34 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace Deondazy\App\Middleware;
+
+use Slim\Views\Twig;
+use Odan\Session\SessionInterface;
+use Psr\Http\Server\MiddlewareInterface;
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Psr\Http\Server\RequestHandlerInterface as RequestHandler;
+
+class FlashValidationErrorMiddleware implements MiddlewareInterface
+{
+    public function __construct(
+        private readonly Twig $twig,
+        private SessionInterface $session
+    )
+    {}
+
+    public function process(Request $request, RequestHandler $handler): Response
+    {
+        if ($this->session->has('errors')) {
+            $errors = $this->session->get('errors');
+
+            $this->twig->getEnvironment()->addGlobal('errors', $errors);
+            
+            $this->session->delete('errors');
+        }
+        
+        return $handler->handle($request);
+    }
+}
