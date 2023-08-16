63798bff8d095d2ad7a559b587f6a75ca7d3ecaa
Deon Okonkwo <deondazy@gmail.com>
2023-08-16 11:32:58 +0100

    chore: env environment loader

---
+++ src/Environment/EnvironmentLoader.php
@@ -0,0 +1,39 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace Deondazy\Core\Environment;
+
+use PhpOption\Option;
+use Deondazy\Core\Environment\EnvironmentLoaderInterface;
+use Dotenv\Repository\RepositoryInterface;
+
+class EnvironmentLoader implements EnvironmentLoaderInterface
+{
+    public function __construct(private readonly RepositoryInterface $repository)
+    {
+    }
+
+    public function get(string $key, $default = null)
+    {
+        return Option::fromValue($this->repository->get($key))
+            ->map(function ($value) {
+                switch (strtolower($value)) {
+                    case 'true':
+                    case '(true)':
+                        return true;
+                    case 'false':
+                    case '(false)':
+                        return false;
+                    case 'empty':
+                    case '(empty)':
+                        return '';
+                    case 'null':
+                    case '(null)':
+                        return null;
+                }
+                return $value;
+            })
+            ->getOrElse($default);
+    }
+}
