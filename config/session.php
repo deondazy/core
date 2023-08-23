0fcd865385b35bed62a043c63cc2ed15cfe3ba45
Deon Okonkwo <deondazy@gmail.com>
2023-08-23 12:41:41 +0100

    update session config

--- config/session.php
+++ config/session.php
@@ -4,11 +4,11 @@ declare(strict_types = 1);
 
 return [
     'name' => 'core_web_session',
-    'lifetime' => 7200,
+    'lifetime' => 120,
     'path' => '/',
     'domain' => null,
     'secure' => true,
     'httponly' => true,
     'cache_limiter' => 'nocache',
-    'same_site' => 'lax',
+    'samesite' => 'lax',
 ];
