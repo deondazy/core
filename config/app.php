a9364b5f3c27608955ebb84dfdd2f303635554dd
Deon Okonkwo <deondazy@gmail.com>
2023-08-23 12:41:08 +0100

    use env function and update style

--- config/app.php
+++ config/app.php
@@ -3,10 +3,10 @@
 declare(strict_types = 1);
 
 return [
-    'name' => 'env:APP_NAME, Core Web',
-    'version' => 'env:APP_VERSION',
-    'url' => 'env:APP_URL, http://localhost',
-    'key' => 'env:APP_KEY, 1234567890',
-    'debug' => false,
+    'name'        => env('APP_NAME', 'DenoSys Core'),
+    'version'     => env('APP_VERSION'),
+    'url'         => env('APP_URL', 'http://localhost'),
+    'key'         => env('APP_KEY'),
+    'debug'       => false,
     'vite_server' => 'http://localhost:5173',
 ];
