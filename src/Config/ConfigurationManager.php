e5eca0afa2fb2d619c91a7b9ada7a0e9c69e998a
Deon Okonkwo <deondazy@gmail.com>
2023-08-23 12:47:20 +0100

    update config manager

--- src/Config/ConfigurationManager.php
+++ src/Config/ConfigurationManager.php
@@ -4,15 +4,8 @@ declare(strict_types = 1);
 
 namespace Denosys\Core\Config;
 
-use Denosys\Core\Environment\EnvironmentLoaderInterface;
-
 class ConfigurationManager
 {
-
-    public function __construct(private EnvironmentLoaderInterface $environmentLoader)
-    {
-    }
-
     public function loadConfigurationFiles(string $directory): array
     {
         $config = [];
@@ -20,7 +13,7 @@ class ConfigurationManager
         foreach ($this->findConfigurationFiles($directory) as $filename) {
             try {
                 $key = pathinfo($filename, PATHINFO_FILENAME);
-                $configFile = require $directory . $filename;
+                $configFile = require_once $directory . $filename;
 
                 $config[$key] = $configFile;
             } catch (\Throwable $e) {
@@ -34,7 +27,7 @@ class ConfigurationManager
 
     private function findConfigurationFiles(string $directory): array
     {
-        return array_filter(scandir($directory), function ($filename) {
+        return array_filter(scandir($directory), function (string $filename) {
             return pathinfo($filename, PATHINFO_EXTENSION) === 'php';
         });
     }
